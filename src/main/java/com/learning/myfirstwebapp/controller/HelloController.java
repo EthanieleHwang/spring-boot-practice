ff5e896938b2ce09e3fbc4d108520f213082403a
ethanhwang <1055256192@qq.com>
2025-05-13 11:15:53 +0800

    Day 2 Spring MVC: Created first Spring Boot Web App with @RestController and @GetMapping

---
+++ src/main/java/com/learning/myfirstwebapp/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.learning.myfirstwebapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        System.out.println("Hello Controller: sayHello() method was called");
+        return "Hello Spring Boot! This is my first web api";
+    }
+
+    @GetMapping("/greet")
+    public String greetSomeone(){
+        return "Greetings,brave explorer!";
+    }
+}
